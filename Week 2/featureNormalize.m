f84c141a279cff922be27dd6b6bbc55ef00d22d2
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2020-12-14 22:29:20 +0530

    Create featureNormalize.m

---
+++ Week 2/featureNormalize.m
@@ -0,0 +1,21 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+
+% You need to set these values correctly
+X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));
+
+
+mu = mean(X_norm);
+sigma = std(X_norm);
+
+a = X_norm - repmat(mu,length(X_norm),1);
+b = repmat(sigma,length(X_norm),1);
+X_norm = a./b;
+
+end
